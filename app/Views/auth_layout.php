7f6debde44a86def4a6cc1641b0fb4d64a4dde2d
Nguyễn Quốc Huy <117934989+tinonguyen15@users.noreply.github.com>
2026-08-05 01:28:17 +0700

    Nạp CSS riêng cho trang đăng nhập

--- app/Views/auth_layout.php
+++ app/Views/auth_layout.php
@@ -5,8 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?= e($title ?? config('app.name')) ?></title>
   <link rel="stylesheet" href="<?= e(url('/assets/css/app.css')) ?>">
+  <link rel="stylesheet" href="<?= e(url('/assets/css/login.css?v=20260805-1')) ?>">
 </head>
-<body class="auth-body">
+<body class="auth-body login-auth-body">
   <?= $content ?>
 </body>
 </html>
