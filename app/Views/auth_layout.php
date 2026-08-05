c9f6c30fda372f46ebbc59e26ad408c4abc1c6a6
Nguyễn Quốc Huy <117934989+tinonguyen15@users.noreply.github.com>
2026-08-05 10:53:36 +0700

    Load font polish on auth layout

--- app/Views/auth_layout.php
+++ app/Views/auth_layout.php
@@ -7,6 +7,7 @@
   <title><?= e($title ?? config('app.name')) ?></title>
   <link rel="stylesheet" href="<?= e(asset_url('/assets/css/app.css')) ?>">
   <link rel="stylesheet" href="<?= e(asset_url('/assets/css/login.css')) ?>">
+  <link rel="stylesheet" href="<?= e(asset_url('/assets/css/font-polish.css')) ?>">
 </head>
 <body class="auth-body login-auth-body">
   <?= $content ?>
